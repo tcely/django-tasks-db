8ad08afe5e40bf3c334ba00888b0a4b9d6b141d9
tcely <tcely@users.noreply.github.com>
2026-09-05 12:37:20 -0400

    Refactor task retrieval logic in db_worker.py

--- django_tasks_db/management/commands/db_worker.py
+++ django_tasks_db/management/commands/db_worker.py
@@ -156,14 +156,15 @@ class Worker:
                 )
             for qn, counts in queue_status.items():
                 a_int, t_int = counts
-                if t := next(
+                t = next(
                     (
                         t
                         for t in running_tasks
                         if task_id == str(t.id) and qn == str(t.queue_name)
                     ),
                     None,
-                ) is None or t_int != len(set(t.worker_ids)):
+                )
+                if t is None or t_int != len(set(t.worker_ids)):
                     # Not all workers were evaluated
                     continue
                 # Reset pings
